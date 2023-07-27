1f6a4cd041eaee2a86c7be074132268ad49d0551
yeojin lee <xlzt2002@gmail.com>
2023-07-27 09:06:23 +0900

    Time: 2 ms (8.76%), Space: 43.6 MB (6.23%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int result = 0;
+        for(int[] account : accounts){
+          result = Math.max(result, Arrays.stream(account).sum());
+        }
+        return result;
+    }
+}
